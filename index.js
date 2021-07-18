a031ed1eaea271f9562e51693664c6605005ca06
ttanimarefin <ttanimarefin@gmail.com>
2021-07-18 11:58:16 +0600

    add new phones section

--- index.js
+++ index.js
@@ -35,5 +35,23 @@ $(document).ready(function(){
         $grid.isotope({ filter: filterValue});
     })
 
+    // new phones owl carousel
+    $("#new-phones .owl-carousel").owlCarousel({
+        loop: true,
+        nav: false,
+        dots: true,
+        responsive : {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000 : {
+                items: 5
+            }
+        }
+    });
+
 
 });
